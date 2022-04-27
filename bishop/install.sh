24c5c95ac4f17155857295fe7eaeb5bb8e9e7596
Johan Egneblad <johan@egneblad.se>
2022-04-27 13:13:04 +0200

    Fix the bishop install script to fit this file structure

--- bishop/install.sh
+++ bishop/install.sh
@@ -2,11 +2,9 @@
 
 HERE=$(pwd)
 
-cd $HERE/files
-
-if >/dev/null /usr/bin/which gcc; then
+if >/dev/null which gcc; then
     echo "Building bishop..."
     gcc bishop.c -o bishop
     chmod +x bishop
-    ln -s --force $HERE/files/bishop $HOME/bin/bishop
+    ln -s --force $HERE/bishop $HOME/bin/bishop
 fi
