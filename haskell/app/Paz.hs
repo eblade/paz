5d096977a019a1b875e04ee986bcbcde1e75a96a
Johan Egneblad <johan@egneblad.se>
2023-07-23 21:09:12 +0200

    The recersive function could be local

--- haskell/app/Paz.hs
+++ haskell/app/Paz.hs
@@ -15,12 +15,12 @@ cutToString length = C.unpack . (B.take length)
 
 pazify :: (ByteString -> ByteString) -> (Int -> ByteString -> Bool) -> ByteString -> (Int, ByteString)
 pazify = pazify' 1
-pazify' :: Int -> (ByteString -> ByteString) -> (Int -> ByteString -> Bool) -> ByteString -> (Int, ByteString)
-pazify' n f p xn_1
-    | p n xn = (n, xn)
-    | otherwise = pazify' (n+1) f p xn
     where
-        xn = f xn_1
+        pazify' n f p xn_1
+            | p n xn = (n, xn)
+            | otherwise = pazify' (n+1) f p xn
+            where
+                xn = f xn_1
 
 calculate :: Int -> ByteString -> ByteString
 calculate length s = B.map translate (Base64.encode digest)
@@ -29,35 +29,27 @@ calculate length s = B.map translate (Base64.encode digest)
         ctx = SHA512.update ctx0 s
         ctx0 = SHA512.init
 
-translate :: Word8 -> Word8
-translate 43 = 57 -- + -> 9
-translate 47 = 56 -- / -> 8 
-translate 61 = 65 -- = -> A
-translate x = x
+        translate :: Word8 -> Word8
+        translate 43 = 57 -- + -> 9
+        translate 47 = 56 -- / -> 8 
+        translate 61 = 65 -- = -> A
+        translate x = x
 
 check :: Int -> Int -> Int -> ByteString -> Bool
-check plength miniterations n x = enough && (startsWithLowerCase pw) && (hasTheStuff pw)
+check plength miniterations n x = enough && (startsWithLowerCase pw) && (hasUpperAndNumber pw)
     where
         enough = (n >= miniterations)
         pw = B.take plength x
-
-startsWithLowerCase :: ByteString -> Bool
-startsWithLowerCase x = ((B.head x) >= 97) && ((B.head x) <= 122)
-
-hasTheStuff :: ByteString -> Bool
-hasTheStuff = hasTheStuff' False False
-hasTheStuff' :: Bool -> Bool -> ByteString -> Bool
-hasTheStuff' hasUpper hasNumber x
-    | B.null x = False
-    | hasNumber && isUpper = True
-    | hasUpper && isNumber = True
-    | otherwise = hasTheStuff' (hasUpper || isUpper) (hasNumber || isNumber) (B.tail x)
-    where
-        isUpper = wordIsUpper x0
-        isNumber = wordIsNumber x0
-        x0 = B.head x
-
-wordIsUpper :: Word8 -> Bool 
-wordIsUpper x = x >= 65 && x <= 90
-wordIsNumber :: Word8 -> Bool 
-wordIsNumber x = x >= 48 && x <= 57
+        startsWithLowerCase x = ((B.head x) >= 97) && ((B.head x) <= 122)
+        hasUpperAndNumber = f False False
+            where
+            f hasUpper hasNumber xxs
+                | B.null xxs = False
+                | hasNumber && isUpper = True
+                | hasUpper && isNumber = True
+                | otherwise = f (hasUpper || isUpper) (hasNumber || isNumber) xs
+                where
+                    isUpper = x >= 65 && x <= 90
+                    isNumber = x >= 48 && x <= 57
+                    x = B.head xxs
+                    xs = B.tail xxs
