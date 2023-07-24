b70675e78fa1243cb901ad4a649391ae27f31142
Johan Egneblad <johan@egneblad.se>
2023-07-24 22:21:24 +0200

    Proper optional password prompt

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,46 +1,82 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators     #-}
-
 module Main where
 
-import Options.Generic
+import Options.Applicative
+import Control.Exception
 import System.IO
 import Paz (makeStart, pazify, check, calculate, cutToString)
 import Data.Maybe (fromMaybe)
 
-data Options = Options
-    { mastera :: Maybe String
-    , sitea   :: Maybe String
-    , lengtha :: Maybe Int
-    , miniterationsa :: Maybe Int
-    } deriving (Generic, Show)
-
-instance ParseRecord Options
-
-data Configuration = Configuration {
+data Paz = Paz {
     master :: String,
     site :: String,
-    plength :: Int,
+    length_ :: Int,
     miniterations :: Int }
 
+paz :: Parser Paz
+paz = Paz
+    <$> strOption
+        ( long "master"
+       <> short 'm'
+       <> value "-"
+       <> metavar "PASSWORD"
+       <> help "Your master password" )
+    <*> strOption
+        ( long "site"
+       <> short 's'
+       <> metavar "SITE"
+       <> help "The name of the site" )
+    <*> option auto
+        ( long "length"
+       <> short 'n'
+       <> showDefault
+       <> value 15
+       <> metavar "INT"
+       <> help "Length of the generated password" )
+    <*> option auto
+        ( long "min-iterations"
+       <> short 'i'
+       <> showDefault
+       <> value 10
+       <> metavar "INT"
+       <> help "Minimum number of hash function passes" )
+
+data Password = Given String | Prompt
+
 main :: IO ()
-main = getRecord "Paz"
+main = execParser opts
     >>= completeOptions
     >>= computeResult
     >>= putStrLn
+    where
+        opts = info (paz <**> helper)
+          ( fullDesc
+         <> progDesc "Deterministically generates a password based on input parameters"
+         <> header "paz - an SGP-based password generator" )
 
-completeOptions :: Options -> IO Configuration
-completeOptions options = return Configuration {
-    site=fromMaybe "default" (sitea options),
-    master=fromMaybe "master" (mastera options),
-    plength=fromMaybe 15 (lengtha options),
-    miniterations=fromMaybe 10 (miniterationsa options) }
+completeOptions :: Paz -> IO Paz
+completeOptions options = do
+    newMaster <- getPassword $ toPassword (master options)
+    return options { master = newMaster }
 
-computeResult :: Configuration -> IO String
-computeResult config = return $ cutToString (plength config) result
+toPassword :: String -> Password
+toPassword s = if s == "-"
+    then Prompt
+    else Given s
+
+getPassword :: Password -> IO String
+getPassword p = case p of
+        Prompt -> do 
+            hPutStr stderr "Password: "
+            withoutEcho getLine
+        Given s -> return s
+
+withoutEcho :: IO a -> IO a
+withoutEcho action =
+    finally (hSetEcho stdin False >> action) (hSetEcho stdin True)
+
+computeResult :: Paz -> IO String
+computeResult config = return $ cutToString (length_ config) result
     where start = makeStart (master config) (site config)
-          calculate' = calculate (plength config)
-          (iterations, result) = pazify calculate' (check (plength config) (miniterations config)) start
+          calculate' = calculate (length_ config)
+          (iterations, result) = pazify calculate' (check (length_ config) (miniterations config)) start
 
