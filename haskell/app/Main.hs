b085cde5b62de034a203d5931218573ed1c64061
Johan Egneblad <johan@egneblad.se>
2023-07-24 16:37:24 +0200

    It sort of works with arguments now, but I think I should switch to getopt

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,7 +1,23 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
+
 module Main where
 
+import Options.Generic
 import System.IO
 import Paz (makeStart, pazify, check, calculate, cutToString)
+import Data.Maybe (fromMaybe)
+
+data Options = Options
+    { mastera :: Maybe String
+    , sitea   :: Maybe String
+    , lengtha :: Maybe Int
+    , miniterationsa :: Maybe Int
+    } deriving (Generic, Show)
+
+instance ParseRecord Options
 
 data Configuration = Configuration {
     master :: String,
@@ -10,17 +26,21 @@ data Configuration = Configuration {
     miniterations :: Int }
 
 main :: IO ()
-main = do
-    hSetBuffering stdin LineBuffering
-    putStrLn $ cutToString (plength config) result
-    hPutStrLn stderr (show (iterations) ++ " iterations")
-        where
-            config = Configuration {
-                site="sites",
-                master="master",
-                plength=15,
-                miniterations=10 }
-            start = makeStart (master config) (site config)
-            calculate' = calculate (plength config)
-            (iterations, result) = pazify calculate' (check (plength config) (miniterations config)) start
+main = getRecord "Paz"
+    >>= completeOptions
+    >>= computeResult
+    >>= putStrLn
+
+completeOptions :: Options -> IO Configuration
+completeOptions options = return Configuration {
+    site=fromMaybe "default" (sitea options),
+    master=fromMaybe "master" (mastera options),
+    plength=fromMaybe 15 (lengtha options),
+    miniterations=fromMaybe 10 (miniterationsa options) }
+
+computeResult :: Configuration -> IO String
+computeResult config = return $ cutToString (plength config) result
+    where start = makeStart (master config) (site config)
+          calculate' = calculate (plength config)
+          (iterations, result) = pazify calculate' (check (plength config) (miniterations config)) start
 
