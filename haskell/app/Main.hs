e5184e60b6451fa5452997d99c70a944582df601
Johan Egneblad <johan@egneblad.se>
2023-07-29 23:42:12 +0200

    Reads sites from remote ini file and shows them if no site is given (for auto-complete)

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import Options.Applicative
-import System.IO
 import Paz (makeStart, pazify, check, calculate, cutToString)
+import Config (loadConfigMaybe, getSections, Config)
 import Password (getPassword)
-import Data.Maybe (fromMaybe)
+import System.Directory (getHomeDirectory)
+import System.Exit
+import Data.List (sort)
 
 data Paz = Paz {
     master :: String,
@@ -23,6 +25,7 @@ paz = Paz
     <*> strOption
         ( long "site"
        <> short 's'
+       <> value "-"
        <> metavar "SITE"
        <> help "The name of the site" )
     <*> option auto
@@ -54,12 +57,30 @@ main = execParser opts
 
 completeOptions :: Paz -> IO Paz
 completeOptions options = do
-    newMaster <- getPassword $ master options
-    return options { master = newMaster }
+    homeDirectory <- getHomeDirectory
+    -- localConfig <- loadConfigMaybe $ homeDirectory ++ "/.pazrc"
+    remoteConfig <- loadConfigMaybe $ homeDirectory ++ "/.pazrc.remote"
+    if ((site options) == "-")
+        then printSitesAndExit remoteConfig
+        else do
+            newMaster <- getPassword $ master options
+            return options { master = newMaster }
+
+printSitesAndExit :: Maybe Config -> IO Paz
+printSitesAndExit config = do
+    _ <- mapM putStrLn $ sortedSections
+    exitSuccess
+    where
+        sortedSections = sort sectionsExceptDefault
+        sectionsExceptDefault = filter (\x -> x /= "DEFAULT") sections
+        sections = case config of
+            Nothing -> []
+            Just c -> getSections c
 
 computeResult :: Paz -> IO String
 computeResult config = return $ cutToString (length_ config) result
-    where start = makeStart (master config) (site config)
-          calculate' = calculate (length_ config)
-          (iterations, result) = pazify calculate' (check (length_ config) (miniterations config)) start
+    where
+        start = makeStart (master config) (site config)
+        calculate' = calculate (length_ config)
+        (_, result) = pazify calculate' (check (length_ config) (miniterations config)) start
 
