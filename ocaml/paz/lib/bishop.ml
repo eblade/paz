bac516571c66f2c2385554258b8a3ccd404eac18
Johan Egneblad <johan@egneblad.se>
2024-07-28 23:17:58 +0200

    addd bishop interface

--- ocaml/paz/lib/bishop.ml
+++ ocaml/paz/lib/bishop.ml
@@ -10,39 +10,33 @@ let int_to_direction = function
 let xlim = 17
 let ylim = 9
 let arsz = xlim * ylim
-let symbols = " .o+=*BOX@%&#/^SE"
-let translate l =
-        Array.map (String.get symbols) l
-        |> Array.to_seq
-        |> String.of_seq
-let init = Array.make arsz 0
 
 let print board =
+        let symbols = " .o+=*BOX@%&#/^SE" in
+        let translate l = Array.map (String.get symbols) l
+        |> Array.to_seq
+        |> String.of_seq in
         let put = Printf.fprintf stderr "%s\n" in
-        let rec put_rows start =
-                if start < arsz
-                then let s = translate (Array.sub board start xlim) in
-                        put ("|" ^ s ^ "|"); put_rows (start + xlim)
-                else () in
+        let rec put_rows start = if start < arsz
+        then let s = translate (Array.sub board start xlim) in
+                put ("|" ^ s ^ "|"); put_rows (start + xlim)
+        else () in
         put "\n+--[ RandomArt ]--+";
         put_rows 0;
         put "+-----------------+"
 
 let drunken_walk (s : string) =
         let l = s |> String.to_seq |> Seq.map Char.code |> List.of_seq in
-        let board = init in
+        let board = Array.make arsz 0 in
         let move pos dir =
                 let limit min max x = Int.min max @@ Int.max min x in
-                let limit_x = limit 0 (xlim - 1) in
-                let limit_y = limit 0 (ylim - 1) in
-                let x = pos mod xlim in
-                let y = pos / xlim in
+                let x = pos mod xlim and y = pos / xlim in
                 let (x', y') = match dir with
                 | NW -> (x - 1, y - 1)
                 | NE -> (x + 1, y - 1)
                 | SW -> (x - 1, y + 1)
                 | SE -> (x + 1, y + 1) in
-                (limit_y y') * xlim + (limit_x x')
+                (limit 0 (ylim - 1) y') * xlim + (limit 0 (xlim - 1) x')
         in
         let rec walk pos stage l = match (stage, l) with
         | (_, []) -> ()
---
+++ ocaml/paz/lib/bishop.mli
@@ -0,0 +1,2 @@
+val print : int array -> unit
+val drunken_walk : string -> int array
